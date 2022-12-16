5a9fb4165f9323a1102cfe949694b64becb1420e
Eustache97 <eubomber97@gmail.com>
2022-12-16 03:22:54 +0100

    doing header

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<section>
+    <h1>This is comics page</h1>
+</section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+    <div class="container">
+        <div class="header-div">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo-dc_comics">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li>
+                        <a href="">Characters</a>
+                    </li>
+                    <li>
+                        <a class="active" href="">Comics</a>
+                    </li>
+                    <li>
+                        <a href="">Movies</a>
+                    </li>
+                    <li>
+                        <a href="">Games</a>
+                    </li>
+                    <li>
+                        <a href="">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="">Videos</a>
+                    </li>
+                    <li>
+                        <a href="">Fans</a>
+                    </li>
+                    <li>
+                        <a href="">News</a>
+                    </li>
+                    <li>
+                        <a href="">Shop</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
